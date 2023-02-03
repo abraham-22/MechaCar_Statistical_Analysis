cedcc312998a5df6c5ad22be0bce90efab4304eb
abraham-22 <abramabebe@gmail.com>
2023-02-03 04:42:13 -0600

    part2 commit

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 
+# Part 1: Linear Regression to Predict MPG
+
 # IMPORT file from CSV 
 mechaCar_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 head (mechaCar_table)
@@ -10,3 +12,15 @@ AWD,data=mechaCar_table) #generate multiple linear regression model
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + 
 ground_clearance + AWD,data=mechaCar_table)) #generate summary statistics
+
+# Part 2: Create Visualizations for the Trip Analysis
+
+# IMPORT file from CSV 
+suspension_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+head (suspension_table)
+
+# creates a total_summary dataframe
+total_summary <- summarize(suspension_table, Mean=mean(PSI), Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')  #create summary table
+head (total_summary)
+# creates a lot_summary dataframe
+lot_summary <- suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(suspension_table, Mean=mean(PSI), Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')   
